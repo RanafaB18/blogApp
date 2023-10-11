da671c721f384e6765fc10edb4dff64dbf75c047
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-10-11 18:12:23 +0000

    Added react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import DataContextProvider from "./context/DataContext.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+])
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <DataContextProvider>
-    <App />
+    <RouterProvider router={router}/>
   </DataContextProvider>
 );
